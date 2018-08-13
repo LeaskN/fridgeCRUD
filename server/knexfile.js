decfd586af4cb72290d3fcde0809b38151ba3032
Nicholas Leask <leask.nicholas@gmail.com>
2018-08-13 15:37:55 -0400

    add production dbconnection

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,4 +4,12 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/fridge_contents'
   },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/test-fridge_contents'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
